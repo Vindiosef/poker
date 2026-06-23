719a636b0a44355dfcb2b6c47e72f3f687570617
Vindiosef <ervinsitchon@gmail.com>
2026-06-23 17:36:59 +0100

    deck, cards

---
+++ card.py
@@ -0,0 +1,51 @@
+class Card:
+
+    CLUB = 1
+    DIAMOND = 2
+    HEART = 3
+    SPADE = 4
+
+    SUIT_MAP = {
+        1 : 'C',
+        2 : 'D',
+        3 : 'H',
+        4 : 'S'
+    }
+
+    RANK_MAP = {
+        2 : '2',
+        3 : '3',
+        4 : '4',
+        5 : '5',
+        6 : '6',
+        7 : '7',
+        8 : '8',
+        9 : '9',
+        10 : '10',
+        11 : 'J',
+        12 : 'Q',
+        13 : 'K',
+        14 : 'A'
+    }
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = 14 if rank == 1 else rank
+    
+    def __eq__(self, value):
+        return self.suit == value.suit and self.rank == value.rank
+    
+    def __str__(self):
+        suit = self.SUIT_MAP[self.suit]
+        rank = self.RANK_MAP[self.rank]
+        return f"{suit},{rank}"
+    
+    def to_id(self):
+        rank = 1 if self.rank == 14 else self.rank
+        return rank + 13 * self.suit
+    
+    @classmethod
+    def from_id(cls, card_id):
+        suit = (card_id - 1) // 13 + 1
+        rank = (card_id -1) % 13 + 1
+        return cls(suit,rank)
---
+++ deck.py
@@ -0,0 +1,20 @@
+from card import Card
+import random
+
+class deck:
+
+    def __init__(self, deck_ids):
+        #this allows for deck variations
+        self.deck = [Card.from_id(cid) for cid in deck_ids] if deck_ids else self._setup_52_cards()
+
+    def draw_card(self):
+        return self.deck.pop()
+
+    def draw_cards(self, num):
+        return [self.draw_card() for _ in range(num)]
+    
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def _setup_52_cards(self):
+        return [Card.from_id(cid) for cid in range (1,53)]
